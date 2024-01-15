4262574d2779c49faa61c8547f9de84b2f44df49
Valiance <lucas.ludovic@epitech.eu>
2024-01-15 16:50:38 +0100

    [CODING STYLE] Removing errors in gameloop.c

--- src/gameloop.c
+++ src/gameloop.c
@@ -16,19 +16,26 @@
 #include "attack.h"
 #include "defense.h"
 
+static
+int reset_ping_values(int increment, int *state_changed, int *number_of_ping)
+{
+    if (increment == RESET_STATE) {
+        *state_changed = 0;
+        return SUCCESS;
+    }
+    if (increment == RESET_PING) {
+        *number_of_ping = 0;
+        return SUCCESS;
+    }
+    return FAILURE;
+}
+
 int retrieve_ping(int increment)
 {
     static int number_of_ping = 0;
     static int state_changed = 0;
 
-    if (increment == RESET_STATE) {
-        state_changed = 0;
-        return SUCCESS;
-    }
-    if (increment == RESET_PING) {
-        number_of_ping = 0;
-        return SUCCESS;
-    }
+    reset_ping_values(increment, &state_changed, &number_of_ping);
     if (state_changed == 1) {
         state_changed = 0;
         return END_RETRIEVE;
@@ -52,6 +59,20 @@ void handle_signal(int signal, UNUSED siginfo_t *info, UNUSED void *context)
         retrieve_ping(INCREMENT);
 }
 
+static
+int play_single_turn(player_t *player)
+{
+    if (player->my_turn == TRUE) {
+        if (play_turn(player) == FAILURE)
+            return FAILURE;
+    } else {
+        my_putstr("waiting for enemy's attack...\n");
+        if (receive_attack(player) == FAILURE)
+            return FAILURE;
+    }
+    return SUCCESS;
+}
+
 int loop(player_t *player)
 {
     int game_on = TRUE;
@@ -59,14 +80,8 @@ int loop(player_t *player)
     while (game_on) {
         if (display_user_gameboard(player) == FAILURE)
             return destroy_end(player);
-        if (player->my_turn == TRUE) {
-            if (play_turn(player) == FAILURE)
-                return FAILURE;
-        } else {
-            my_putstr("waiting for enemy's attack...\n");
-            if (receive_attack(player) == FAILURE)
-                return FAILURE;
-        }
+        if (play_single_turn(player) == FAILURE)
+            return FAILURE;
         usleep(1000);
     }
     return SUCCESS;
